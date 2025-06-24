4224c58092e9eab19ac457034d8a9988f3d4b54e
Huu Hoai <daohuuhoai2655@gmail.com>
2025-06-24 15:31:30 +0700

    chore: add example for i18next

--- src/config/i18n.js
+++ src/config/i18n.js
@@ -24,7 +24,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: 'vi',
+    lng: 'en',
     fallbackLng: 'en',
     // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
--- src/screen/new_feed/NewFeedPage.jsx
+++ src/screen/new_feed/NewFeedPage.jsx
@@ -1,6 +1,7 @@
+import { t } from 'i18next';
 import { logger } from '../../util/logger';
 
 export const NewFeedPage = () => {
   logger.trace('NewFeedPage rendered');
-  return <div className='bg-primary-400 h-[30000px] w-full text-9xl'>Hello World</div>;
+  return <div className='bg-primary-400 h-[30000px] w-full text-9xl'>{t('example.hello_world')}</div>;
 };
